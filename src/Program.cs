913a03a4efa84423c253a9dc5c5a417cf2534221
Israel Lot <israel.lot@gmail.com>
2024-03-28 13:22:15 -0300

    add UnsafeArray and benchmark

---
+++ src/Program.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using BenchmarkDotNet;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Running;
+using UnsafeArray;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run(new[] {
+            typeof(SequentialRead),
+            typeof(RandomRead),
+            typeof(RandomWrite)
+        });
+
+        Console.ReadKey();
+    }
+}
+
+[DisassemblyDiagnoser(maxDepth: 100)]
+public class RandomRead
+{
+    [Params(64, 256, 512, 1024, 4096, 1024 * 16, 1024 * 1024, 1024 * 1024 * 16)]
+    public int ArraySize { get; set; }
+
+    private uint[] _safeArray;
+    private UnsafeUIntArray _unsafeArray;
+    private int[] _sequence;
+
+    public RandomRead()
+    {
+        _safeArray = new uint[ArraySize];
+        _unsafeArray = new UnsafeUIntArray (ArraySize);
+        _sequence = new int[ArraySize];
+    }
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        _safeArray = new uint[ArraySize];
+        _unsafeArray = new UnsafeUIntArray (ArraySize);
+
+        var random= new Random(111);
+
+        _sequence = new int[ArraySize];
+        for (var i = 0; i < ArraySize; i++)
+        {
+            _sequence[i] = i;
+
+            var v = (uint)random.Next();
+            _safeArray[i] = v;
+            _unsafeArray[i] = v;
+        }
+
+        random.Shuffle(_sequence);
+    }
+        
+    [Benchmark(Baseline = true)]
+    public uint SafeArray()
+    {
+        uint v = 0;
+
+        foreach (var i in _sequence)
+            v += _safeArray[i];
+
+        return v;
+    }
+        
+    [Benchmark]
+    public uint UnsafeArray()
+    {
+        uint v = 0;
+
+        foreach (var i in _sequence)
+            v += _unsafeArray.Get(i);
+
+        return v;
+    }
+}
+
+[DisassemblyDiagnoser(maxDepth: 100)]
+public class SequentialRead
+{
+    [Params(64,256,512,1024,4096,1024*16,1024*1024, 1024 * 1024 * 16)]
+    public int ArraySize { get; set; }
+
+    private uint[] _safeArray;
+    private UnsafeUIntArray _unsafeArray;
+
+    public SequentialRead()
+    {
+        _safeArray = new uint[ArraySize];
+        _unsafeArray = new UnsafeUIntArray(ArraySize);
+    }
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        _safeArray = new uint[ArraySize];
+        _unsafeArray = new UnsafeUIntArray(ArraySize);
+
+        var random = new Random();
+
+        for (var i = 0; i < ArraySize; i++)
+        {
+            _safeArray[i] = (uint)random.Next();
+            _unsafeArray[i] = _safeArray[i];
+        }
+    }
+
+
+    [Benchmark(Baseline = true)]
+    public uint SafeArray()
+    {
+        uint v = 0;
+
+        var limit = ArraySize;
+
+        for (var i = 0; i < limit; i++)
+            v += _safeArray[i];
+
+        return v;
+    }
+
+
+    [Benchmark]
+    public uint UnsafeArray()
+    {
+        uint v = 0;
+
+        var limit = ArraySize;
+
+        for (var i = 0; i < limit; i++)
+            v += _unsafeArray.Get(i);
+
+        return v;
+    }
+}
+
+[DisassemblyDiagnoser(maxDepth: 100)]
+public class RandomWrite
+{
+    [Params(64, 256, 512, 1024, 4096, 1024 * 16, 1024 * 1024, 1024 * 1024 * 16)]
+    public int ArraySize { get; set; }
+
+    private uint[] _safeArray;
+    private UnsafeUIntArray _unsafeArray;
+    private int[] _sequence;
+
+    public RandomWrite()
+    {
+        _safeArray = new uint[ArraySize];
+        _unsafeArray = new UnsafeUIntArray(ArraySize);
+        _sequence = new int[ArraySize];
+    }
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        _safeArray = new uint[ArraySize];
+        _unsafeArray = new UnsafeUIntArray(ArraySize);
+
+        var random = new Random(111);
+
+        _sequence = new int[ArraySize];
+        for (var i = 0; i < ArraySize; i++)
+        {
+            _sequence[i] = i;
+        }
+        random.Shuffle(_sequence);
+    }
+
+    [Benchmark(Baseline = true)]
+    public void SafeArray()
+    {                        
+        foreach(var i in _sequence)
+        {
+            _safeArray[i] = 111;
+        }
+    }
+
+
+    [Benchmark]
+    public void UnsafeArray()
+    {
+        foreach (var i in _sequence)
+        {
+            _unsafeArray.Set(i, 111);
+        }
+
+    }
+}
+
+
---
+++ src/UnsafeArray.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Buffers.Binary;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace UnsafeArray;
+
+public unsafe class UnsafeUIntArray
+{
+    public int Length { get; init; }
+
+    public uint[] Data { get; init; }
+
+    private readonly uint[] _data;
+    private readonly uint* _dataPtr;
+    public UnsafeUIntArray(int length)
+    {
+        _data = GC.AllocateArray<uint>(length, pinned: true);
+
+        ref uint dataRef = ref MemoryMarshal.GetArrayDataReference(_data);
+
+        _dataPtr = (uint*)Unsafe.AsPointer<uint>(ref dataRef);
+
+        Length = length;
+
+        Data = _data;        
+    }
+   
+    public uint this[int key]
+    {
+        get => Get(key);
+        set => Set(key, value);
+    }    
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public unsafe uint Get(int index)
+    {
+        var ptr = (_dataPtr + (uint)index);
+        return *ptr;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public unsafe void Set(int index, uint value)
+    {
+        var ptr = (_dataPtr + (uint)index);
+
+        *ptr = value;
+    }
+
+}
+
+
